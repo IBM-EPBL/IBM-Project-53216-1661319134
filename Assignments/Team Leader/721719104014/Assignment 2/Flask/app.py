14168c1c2d9d2e1d56bd7cb4ae2f62aca078e5ad
hemasaiburru <113695768+hemasaiburru@users.noreply.github.com>
2022-11-12 21:43:23 +0530

    Add files via upload

---
+++ Assignments/Team Leader/721719104014/Assignment 2/Flask/app.py
@@ -0,0 +1,91 @@
+from flask import Flask, render_template, request
+from markupsafe import escape
+
+import ibm_db
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=2d46b6b4-cbf6-40eb-bbce-6251e6ba0300.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=32328;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=xrl27444;PWD=TR5CfVrO2n3Q92s4",'','')
+
+app = Flask(__name__, template_folder="./Templates")
+
+
+
+@app.route('/')
+def home():
+  return render_template('home_body.html')
+
+@app.route('/signin')
+def home():
+  return render_template('sign_in.html')
+
+@app.route('/signup')
+def home():
+  return render_template('sign_up.html')
+
+@app.route('/addstudent')
+def new_student():
+  return render_template('add_student.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+  if request.method == 'POST':
+
+    name = request.form['name']
+    address = request.form['address']
+    city = request.form['city']
+    pin = request.form['pin']
+
+    sql = "SELECT * FROM students WHERE name =?"
+    stmt = ibm_db.prepare(conn, sql)
+    ibm_db.bind_param(stmt,1,name)
+    ibm_db.execute(stmt)
+    account = ibm_db.fetch_assoc(stmt)
+
+    if account:
+      return render_template('list.html', msg="You are already a member, please login using your details")
+    else:
+      insert_sql = "INSERT INTO students VALUES (?,?,?,?)"
+      prep_stmt = ibm_db.prepare(conn, insert_sql)
+      ibm_db.bind_param(prep_stmt, 1, name)
+      ibm_db.bind_param(prep_stmt, 2, address)
+      ibm_db.bind_param(prep_stmt, 3, city)
+      ibm_db.bind_param(prep_stmt, 4, pin)
+      ibm_db.execute(prep_stmt)
+    
+    return render_template('home.html', msg="Student Data saved successfuly..")
+
+@app.route('/list')
+def list():
+  students = []
+  sql = "SELECT * FROM Students"
+  stmt = ibm_db.exec_immediate(conn, sql)
+  dictionary = ibm_db.fetch_both(stmt)
+  while dictionary != False:
+    # print ("The Name is : ",  dictionary)
+    students.append(dictionary)
+    dictionary = ibm_db.fetch_both(stmt)
+
+  if students:
+    return render_template("list.html", students = students)
+
+@app.route('/delete/<name>')
+def delete(name):
+  sql = f"SELECT * FROM Students WHERE name='{escape(name)}'"
+  print(sql)
+  stmt = ibm_db.exec_immediate(conn, sql)
+  student = ibm_db.fetch_row(stmt)
+  print ("The Name is : ",  student)
+  if student:
+    sql = f"DELETE FROM Students WHERE name='{escape(name)}'"
+    print(sql)
+    stmt = ibm_db.exec_immediate(conn, sql)
+
+    students = []
+    sql = "SELECT * FROM Students"
+    stmt = ibm_db.exec_immediate(conn, sql)
+    dictionary = ibm_db.fetch_both(stmt)
+    while dictionary != False:
+      students.append(dictionary)
+      dictionary = ibm_db.fetch_both(stmt)
+    if students:
+      return render_template("list.html", students = students, msg="Delete successfully")
+
+  return "success..."
